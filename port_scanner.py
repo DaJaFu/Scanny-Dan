b9752788aeea3b1df8b27efe7796d39fa1314d80
DaJaFu <dnlfssll@gmail.com>
2023-11-26 20:15:21 -0500

    Update port_scanner.py
    Added Features:
    + Multiple port scans at once.
    + Integer filter.
    + User input.

--- port_scanner.py
+++ port_scanner.py
@@ -1,19 +1,34 @@
 #!/usr/bin/env python3
-
+import sys
 import socket
+from typing import List
 
+#Get user input and return the data as a list of strings
+def get_input() -> List[str]:
+    port_input = input('Which port(s) would you like to scan? : ')
+    return port_input.split(',')
 
 #defining the port scan function, and setting parameters.
-def port_scan(target_ip, port):
+def port_scan(target_ip: str, ports: List[str]):
     #iterating over all the desired ports in the target network and attempting to open a socket with them.
-    sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    sock.settimeout(1)
-    try:
-        sock.connect((target_ip,port))
-        print "Port is open."
-    except:  
-        print "Port is closed."
-    
-
-
-port_scan('localhost',input("Which port would you like to scan? : "))
+    for port_string in ports:
+        #first, attempt to translate the string into an integer
+        try:
+            port = int(port_string)
+        #if that doesn't work, skip the item in the list and print an error message.
+        except ValueError:
+            print(f"ERROR Expected integer: {port_string}")
+            continue
+        #creating a socket using ipv4 (AF_INET)
+        sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        #if the socket can't connect to a port within this time allotment, quit
+        sock.settimeout(1)
+        #attempt to connect to the current port, if unable, return that it's closed
+        try:
+            sock.connect((target_ip,port))
+            print(f"Port {port} is open.")
+        except:  
+            print(f"Port {port} is closed.")
+#prints command line args for later usage
+print(sys.argv)
+port_scan('localhost',get_input())
