f30f179412c90981fe4eb7fc585f74d83e36cb4a
DaJaFu <dnlfssll@gmail.com>
2023-12-14 13:37:28 -0500

    Port scanner input update
    + Now able to parse files for potential port numbers using -f or --file.
    
    + Changed method of acquiring command line input into its own function and used a dictionary, will probably change this later.
    
    + Simplified some stuff.

--- port_scanner.py
+++ port_scanner.py
@@ -14,24 +14,61 @@ def get_service_name(port):
         return service
     except:
         return "Unknown"
+    
+#get input type through command line input
+def get_type() -> str:
+    arg_dict = {'-f':'f','--file':'f','-p':'p','--port':'p','-r':'r','--range':'r','-h':'h','--help':'h'}
+    if sys.argv[1] in arg_dict:
+        return arg_dict.get(sys.argv[1])
+    else:
+        return None
 
-#Get user input and return the data as a list of strings
-def get_input() -> List[str]:
+#open a file specified in the command line and search for valid port numbers
+def parse_file(filename)->List[str]:
+    file = open(filename,'r')
+    read_file = file.read()
     port_list = []
-    for argument in sys.argv:
-        if argument in ('-f','--file'):
-            port_f = open(str(sys.argv[2]),'r')
-            for port in port_f.read().split(','):
-                port_list.append(port)
-        elif argument in ('-p','--port'):
-            port_list.append(str(sys.argv[2]))
-        elif argument in ('-r','--range'):
-            range_l = str(sys.argv[2]).split('-')
-            for i in range(int(range_l[0]),int(range_l[1])+1):
-                port_list.append(i)
-        elif argument in ('-h','--help'):
-            print(usage.read())
-            return 'h'
+    current_port = ''
+    for i in range(len(read_file)):
+        #print(read_file[i])
+        #if the current iterator is equal to the len of the file, quit
+        if i == len(read_file):
+            break
+        #if current character can be converted to an integer (is an int string)
+        try:
+            int(read_file[i])
+            #print(read_file[i])
+            #add current character to the current port string
+            current_port = current_port + str(read_file[i])
+        except:
+            continue
+        #if the next character is a number then skip to it
+        if len(current_port)<5:
+            try:
+                #print(read_file[i+1])
+                int(read_file[i+1])
+            except:
+                #print(current_port)
+                port_list.append(current_port)
+                current_port = ''
+        else:
+            port_list.append(current_port)
+            current_port = ''
+    return port_list
+
+#print(parse_file(sys.argv[2]))
+
+#Parse user input and return data as a list of strings
+def get_ports_from_input() -> List[str]:
+    port_list = []
+    if get_type() == 'f':
+        port_list = parse_file(sys.argv[2])
+    elif get_type() == 'p':
+        port_list.append(str(sys.argv[2]))
+    elif get_type() == 'r':
+        range_l = str(sys.argv[2]).split('-')
+        for i in range(int(range_l[0]),int(range_l[1])+1):
+            port_list.append(i)
     return port_list
 
 #defining the port scan function, and setting parameters.
@@ -62,9 +99,9 @@ def port_scan(target_ip: str, ports: List[str]):
 
 #if this script is the main program being run, attempt to scan ports using get_input
 if __name__ == "__main__":
-    if get_input() != 'h':    
-        try:
-            port_scan('localhost',get_input())
-        except:
-            print(f'ERROR! Port input received: {get_input()}')
-            print(usage.read())
+    if get_type() == None:
+        print(f'ERROR! {" ".join(sys.argv)} is not acceptable input. Try using -h for more info.')
+    elif get_type() == 'h':
+        print(usage.read())
+    else:
+        port_scan('localhost',get_ports_from_input())
